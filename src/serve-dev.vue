7a2e8653305c0d260f4d5b02690d0a7320d90889
Alex Tjahjana <a.tjahjana@auckland.ac.nz>
2020-03-17 14:05:09 +1300

    commit to test npm publishing

---
+++ src/serve-dev.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VuejsTest from '@/vuejs-test.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VuejsTest
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vuejs-test />
+  </div>
+</template>
